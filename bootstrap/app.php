7190ec29a44732997c33fdde3ff8b8aab2a702cf
MaulinMakwana <maulinmakwana.aditech@gmail.com>
2025-11-27 18:26:04 +0530

    Minor changes for CSRF Token error in frontend

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->statefulApi();
+        //$middleware->statefulApi();
         $middleware->alias([
             'token.expiry' => \App\Http\Middleware\CheckTokenExpiry::class,
             'check.superadmin' => \App\Http\Middleware\CheckSuperAdmin::class,
