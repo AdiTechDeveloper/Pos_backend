b0a750f43ae9d3af530e7088c1433b4f6deae5be
MaulinMakwana <maulinmakwana.aditech@gmail.com>
2025-12-24 16:43:34 +0530

    Minor changes for cors

--- config/cors.php
+++ config/cors.php
@@ -2,23 +2,23 @@
 
 return [
 
-'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-'allowed_methods' => ['*'],
+    'allowed_methods' => ['*'],
 
-'allowed_origins' => [
-    'http://localhost:3000',
-     'http://127.0.0.1:3000',
-    'https://loyality.theaditech.in', // Add your frontend URL here
-],
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'http://127.0.0.1:3000',
+        'https://loyality.theaditech.in', // Add your frontend URL here
+    ],
 
-'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [],
 
-'allowed_headers' => ['*'],
+    'allowed_headers' => ['*'],
 
-'exposed_headers' => [],
+    'exposed_headers' => [],
 
-'max_age' => 0,
+    'max_age' => 0,
 
-'supports_credentials' => true, // Set to true if using Sanctum or Cookies
+    'supports_credentials' => true, // Set to true if using Sanctum or Cookies
 ];
